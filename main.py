edd8a86b7bed7a9c2a26685cda242f8e4583c18d
TsukasaMita <tsukasa.mita@gmail.com>
2026-02-26 11:11:16 +0100

    add scoreboard class and update the score if it collide with the food

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 from turtle import Screen
+from scoreboard import Scoreboard
 from snake import Snake
 from food import Food
 
@@ -11,6 +12,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 
 screen.listen()
@@ -31,6 +33,7 @@ while game_is_on:
     # Detect collision with food.
     if snake.head.distance(food) < 15:
         food.refresh()
+        scoreboard.increase_score()
 
 
 
